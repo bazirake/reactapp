7db7b25b80ce869440a9610e9f04e8bc28e29fb6
bazirake <eric.bazirake@abr.rw>
2025-04-01 17:00:06 +0200

    hello color

--- src/Components/NavBars.tsx
+++ src/Components/NavBars.tsx
@@ -44,7 +44,7 @@ function NavBars(){
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-success" type="submit">Search</button>
+              <button className="btn btn-primary" type="submit">Search</button>
             </form>
           </div>
         </div>
