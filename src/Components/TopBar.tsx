c6130e72afe2bedfb296bbf16d4aacd6a35be9de
bazirake <eric.bazirake@abr.rw>
2025-04-03 16:17:16 +0200

    topbar changes

--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -4,9 +4,13 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 
 function TopBar() {
   return (
-    <div className="top-wrapper">
-   <i className="bi bi-bell-fill px-2"></i>
-   <span>Exceptional Tech Company calls up you all to follow its products</span>
+    <div className="d-flex bd-highlight top-wrapper">
+      <div className="flex-fill p-2 bd-highlight">
+      <span className="px-2"><i className="bi bi-telephone"></i>250787124101</span>
+      <span><i className="bi bi-envelope"></i>info@exceptionaltech.rw</span>
+      </div>
+    
+   <span className="p-2 flex-fill bd-highlight "> <i className="bi bi-bell-fill px-1"></i> Exceptional Tech Company calls up you all to follow its products</span>
   </div>
   )
 }
