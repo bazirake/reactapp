bc24b7d43c90f291fd7d6098cca3c5509037d992
bazirake <eric.bazirake@abr.rw>
2025-04-12 16:15:23 +0200

    gallery changes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -320,6 +320,98 @@ export default function Home() {
        </div>
         </section>
 
+        <section id="gallery" className="gallery m-section">
+         {/* start of section title  */}
+        <div className="container section-title aos-init aos-animate" data-aos="fade-up">
+        <h2>Gallery</h2>
+        <div><span>Check ExTech</span> <span className="description-title">Gallery</span></div>
+      </div>
+       {/* End Section Title */}
+
+       <div className="container aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+
+        <div className="row g-0">
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-1.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-1.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          {/* End Gallery Item */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-2.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-2.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          
+           {/* End Gallery Item */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-3.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-3.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          {/* End Gallery Item */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-4.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-4.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          
+          {/* End Gallery Item  */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-4.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-4.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+           {/* End Gallery Item */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-5.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-5.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          
+          {/* End Gallery Item */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="	https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-6.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-6.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+          {/* End Gallery Item  */}
+
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-7.jpg" className="glightbox" data-gallery="images-gallery">
+                <img src="https://bootstrapmade.com/content/demo/Bootslander/assets/img/gallery/gallery-7.jpg" alt="" className="img-fluid"/>
+              </a>
+            </div>
+          </div>
+           {/* End Gallery Item */}
+
+        </div>
+
+      </div>
+        </section>
+
        <section className="web-back">
        <div className="container-fluid">
           <div className="row g-0">
