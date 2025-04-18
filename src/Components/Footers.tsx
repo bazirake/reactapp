84c05f1d2a80fcef770e578862349472df607056
bazirake <eric.bazirake@abr.rw>
2025-04-18 23:12:35 +0200

    commit subscribe button

--- src/Components/Footers.tsx
+++ src/Components/Footers.tsx
@@ -1,5 +1,6 @@
 
 import "../Components/Navbars.css";
+import "../Components/Footer.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css"
 import { NavLink } from 'react-router';
@@ -8,12 +9,12 @@ function Footers() {
     <footer className="footer-color text-dark py-4">
   <div className="container">
     <div className="row">
-      <div className="col-md-4">
+      <div className="col-lg-3  col-md-4">
         <h5>About Us</h5>
-        <p className="text-dec">ETC is Exeptional Techcompany aiming at delivering tech Services than wherever you can find</p>
+        <p className="text-dec">ExTech is Exeptional Techcompany aiming at delivering tech Services than wherever you can find</p>
       </div>
-      <div className="col-md-4">
-        <h5>Quick Links</h5>
+      <div className="col-lg-3  col-md-4">
+        <h5>Useful Links</h5>
         <ul className="list-unstyled">
           <li><NavLink to={`/`} className="text-dark text-dec">Home</NavLink></li>
           <li><NavLink to={`/service`} className="text-dark text-dec">Services</NavLink></li>
@@ -21,7 +22,7 @@ function Footers() {
           <li><NavLink to={`/product`} className="text-dark text-dec">Product</NavLink></li>
         </ul>
       </div>
-      <div className="col-md-4">
+      <div className=" col-lg-3  col-md-4">
         <h5>Follow Us</h5>
         <NavLink to="#" className="me-3 text-dec"><i className="bi bi-facebook "></i></NavLink>
         <NavLink to="#" className="me-3 text-dec"><i className="bi bi-twitter-x"></i></NavLink>
@@ -33,6 +34,23 @@ function Footers() {
         <br></br>
         <p className="text-dec">&copy; 2025 E.Tech Company. All rights reserved.</p>
       </div>
+
+      <div className="col-lg-3 col-md-4">
+      <h5>ExTech Ltd</h5>
+
+      <p>Subscribe to ExTech Ltd and receive the latest Updates about our products and services!</p>
+      <form  method="post" className="php-email-form">
+
+      <div className="newsletter-form">
+        <input type="email" className="form-control" name="email"/>
+        <button type="button">Subscribe</button></div>
+            
+            <div className="loading">Loading</div>
+            <div className="error-message"></div>
+            <div className="sent-message">Your subscription request has been sent. Thank you!</div>
+          </form>
+      </div>
+
     </div>
   </div>
 </footer>
