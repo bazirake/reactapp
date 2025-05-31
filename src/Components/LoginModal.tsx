d086c57070cc2dbcff785f81c1f9187c6cdb9379
bazirake <eric.bazirake@abr.rw>
2025-05-31 14:56:16 +0200

    changes form  data

--- src/Components/LoginModal.tsx
+++ src/Components/LoginModal.tsx
@@ -1,7 +1,7 @@
 import "../Components/Loginmodell.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../Components/sign.css"
-import React from 'react'
+import React, { useState } from 'react'
 interface RegisterProps{
   handleRegister:()=>void;
   idc:any;
@@ -9,42 +9,57 @@ interface RegisterProps{
 }
 
 function LoginModal({handleRegister,idc,idco}:RegisterProps) {
+              type  formdata={
+                    id1:string;
+                    id2:string;
+                    fname:string;
+                    emails:string;
+                    phone:string;
+                    country:string,
+                    terms:boolean;
+                }
+  const [coursedata,setCourses]=useState<formdata>({id1:idc,id2:idco,fname:"",emails:"",phone:"",country:"",terms:false})
   return (
  <div id="popup" className="modal">
               <div className="card card-m ">
-            <div className="card-header panel-color">
+              <div className="card-header panel-color">
                <span className="float-end close" onClick={handleRegister}>X</span>
-             <h4 className='text-center'>Sign in</h4> 
-            
-            </div>
+               <h4 className='text-center'>Sign in</h4> 
+              </div>
             <div className="card-body">
-                <form>
+                <form onSubmit={
+                  (e)=>{
+                    e.preventDefault();
+                    alert(coursedata.terms)
+                   //console.log(coursedata);
+                 }
+                }>
                   <div className='form-group'>
-                    <input value={idc} type='text' className='form-control form-field' placeholder='Enter full names' disabled></input>
-                    <input value={idco} type='text' className='form-control form-field' placeholder='Enter full names' disabled></input>
+                    <input value={coursedata.id1}  type='text'  className='form-control form-field' placeholder='Enter full names' disabled></input>
+                    <input value={coursedata.id2} type='text'  className='form-control form-field' placeholder='Enter full names' disabled></input>
                     <label >Fullname</label>
-                    <input type='text' className='form-control form-field' placeholder='Enter full names'></input>
+                    <input type='text' onChange={(e)=>setCourses((prevadata)=>({...prevadata,fname:e.target.value}))}  value={coursedata.fname} className='form-control form-field' placeholder='Enter full names'></input>
                     </div>
                     <div className='form-group'>
                     <label>Email</label>
-                    <input type='email' placeholder='Enter email' className='form-control form-field'></input>
+                    <input type='email' onChange={(e)=>setCourses((prevdata)=>({...prevdata,emails:e.target.value}))} placeholder='Enter email' className='form-control form-field'></input>
                     </div>
                     <div className='form-group'>
                     <label>Phone number</label>
-                    <input type='text' placeholder='Enter phone number' className='form-control form-field'></input>
+                    <input type="text" onChange={(e)=>setCourses((prevdata)=>({...prevdata,phone : e.target.value}))} value={coursedata.phone} placeholder='Enter phone number' className='form-control form-field'></input>
                     </div>
                     <div className='form-group'>
                     <label>Select country</label>
-                    <select className='form-control form-field'>
-                      <option value="Rwanda">Rwanda</option>
-                      <option value="Burundi">Burundi</option>
-                      <option value="Tanzania">Tanzania</option>
-                      <option value="Uganda">Uganda</option>
-                      <option value="Kenya">Kenya</option>
+                    <select className='form-control form-field' onChange={(e)=>setCourses((prevdata)=>({...prevdata,country:e.target.value}))}>
+                        <option value="Rwanda">Rwanda</option>
+                        <option value="Burundi">Burundi</option>
+                        <option value="Tanzania">Tanzania</option>
+                        <option value="Uganda">Uganda</option>
+                        <option value="Kenya">Kenya</option>
                     </select>
                     </div>
                     <div className="custom-control custom-checkbox my-2">
-                    <input type="checkbox" className="custom-control-input" id="customCheckDisabled" />
+                    <input type="checkbox" checked={coursedata.terms} onChange={(e)=>setCourses((prevdata)=>({...prevdata,terms:e.target.checked}))} className="custom-control-input" id="customCheckDisabled" />
                    <label className="custom-control-label">Accept terms and conditions</label>
                  </div>
                  <div className='d-flex align-items-center justify-content-center'><button type='submit'   className='btn btn-color-service my-2 mx-auto w-50'>RegisterNow</button></div>
