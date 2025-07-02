5453815ac82a2e978fbd18afe3b9d940080a678b
P@ssword123455 <bazirakee2@gmail.com>
2025-07-02 23:15:59 +0200

    changes data

--- src/Components/Footers.tsx
+++ src/Components/Footers.tsx
@@ -3,9 +3,33 @@ import "../Components/Navbars.css";
 import "../Components/Footer.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css"
-import { NavLink } from 'react-router';
+import {NavLink} from 'react-router';
+import { useState } from "react";
+
+     type Subobject={
+       email:string
+     } 
+
+     type EmailError={
+      emError?:string
+     }                                                
+                                                      
 function Footers() {
-  return (
+
+ const[emails,setEmail]=useState<Subobject>({email:''});
+ const[errem,setErroremail]=useState<EmailError>({});
+ const ValidateSub=()=>{
+ const errdata:EmailError={};
+  if(!emails.email){
+    errdata.emError="Email field shoud not be empty";
+  }
+  if(!/\S+@\S+\.\S+/.test(emails.email)){
+    errdata.emError="Please enter a valid email";                 
+    }
+   setErroremail(errdata);
+   return Object.keys(errdata).length===0
+}
+  return(
     <footer className="footer-color text-dark py-4">
   <div className="container">
     <div className="row">
@@ -31,20 +55,27 @@ function Footers() {
         <a href=""  className="me-3 text-dec"><i className="bi bi-linkedin"></i></a>
         <a href=""  className="me-3 text-dec"><i className="bi bi-instagram"></i></a>
         <a href=""  className="me-3 text-dec"><i className="bi bi-youtube"></i></a>      
-        <p className="text-dec">&copy; 2025 ExTech Company. All rights reserved.</p>
+        <p className="text-dec">&copy;2025 ExTech Company.All rights reserved.</p>
       </div>
-
       <div className="col-lg-3 col-md-4">
       <h5>ExTech Ltd</h5>
-
       <p className="text-dec">Subscribe to ExTech Ltd and receive the latest Updates about our products and services!</p>
-      <form  method="post" className="php-email-form">
+      <form  onSubmit={(e)=>{
+          e.preventDefault();
+          if (ValidateSub()) {
+             alert(emails.email);
+          }
+          else{
+            alert(errem.emError);
+          }
+          
+      }} method="post" className="php-email-form">
        <div className="newsletter-form">
-        <input type="email" className="form-control" name="email"/>
-         <button type="button">Subscribe</button></div>
+        <input type="text" value={emails.email} onChange={(e)=>setEmail((prevemail)=>({...prevemail,email:e.target.value}))} className="form-control" name="email"/>
+         <button type="submit">Subscribe</button></div>
             <div className="loading">Loading</div>
-            <div className="error-message"></div>
-            <div className="sent-message">Your subscription request has been sent. Thank you!</div>
+             <div className="error-message"></div>
+             <div className="sent-message">Your subscription request has been sent.Thank you!</div>
           </form>
       </div>
     </div>
--- src/Components/Subfooter.tsx
+++ src/Components/Subfooter.tsx
@@ -6,32 +6,23 @@ import React, { useEffect, useState } from 'react'
 import { Baseline } from 'lucide-react';
 
 export default function Subfooter() {
-
-  const [scrolledDown, setScrolledDownx] = useState(false);
+  const [scrolledDown,setScrolledDownx] = useState(false);
     useEffect(() => {
       const handleScrollx = () => {
         const scrollY = window.scrollY;
-      
-        if (scrollY >100) {
+        if (scrollY >100){
           setScrolledDownx(true);
         } else {
           setScrolledDownx(false);
         }
       };
-  
       window.addEventListener('scroll', handleScrollx);
-  
       return () => window.removeEventListener('scroll', handleScrollx);
     }, []);
   
-
-
-
   return (
-    <div className='container-fluid d-flex  sub-margin' style={{backgroundColor:"#eaf3fa"}}>
-     
+    <div className='container-fluid d-flex  sub-margin' style={{backgroundColor:"#eaf3fa"}}> 
      <p className="font-14 fonts-w text-sm p-2 flex-grow-1 text-center">Website by ExTechnology Solutions</p>
-       
             <a href="#" id="scroll-top" className={`d-flex align-items-center justify-content-center ${scrolledDown ? "scroll-top":"active"}`}><i className="bi bi-arrow-up-short"></i></a>
     </div>
   )
